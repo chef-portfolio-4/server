d504bd09f8914dda8dab68c3807da96c3ea9446d
Joe O <j0liverko@gmail.com>
2020-01-06 13:49:57 -0600

    changed to production

--- server.js
+++ server.js
@@ -14,5 +14,7 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 // server.use('/api/jokes', authenticate, jokesRouter);
-
+server.get('/', (req, res) => {
+    console.log("its alive")
+})
 module.exports = server;
